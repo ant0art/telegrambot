a95b71256eaa6f278a095ef19e97ce293dfb1f91
Anton Filatov <antartbox@gmail.com>
2022-08-08 22:20:34 +0300

    Initial commit

--- src/main/java/bot/dompp/Bot.java
+++ src/main/java/bot/dompp/Bot.java
@@ -16,39 +16,47 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class Bot extends TelegramLongPollingBot {
 	private static final Map<String, String> getenv = System.getenv();
 
+	private final String BOT_TOKEN;
+	private final String BOT_NAME;
+
+	public Bot(String botName, String botToken) {
+		this.BOT_NAME = botName;
+		this.BOT_TOKEN = botToken;
+	}
+
 	public static void main(String[] args) {
-        try {
-            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new Bot());
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot(new Bot(getenv.get("BOT_NAME"), getenv.get("BOT_TOKEN")));
 
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public void onUpdateReceived(Update update) {
-        Message incomMess = update.getMessage();
-        String incomMessChatId = incomMess.getChatId().toString();
-        String outMeString = incomMess.getText();
-        SendMessage outMess = new SendMessage();
+	@Override
+	public void onUpdateReceived(Update update) {
+		Message incomMess = update.getMessage();
+		String incomMessChatId = incomMess.getChatId().toString();
+		String outMeString = incomMess.getText();
+		SendMessage outMess = new SendMessage();
 
-        outMess.setChatId(incomMessChatId);
-        outMess.setText(outMeString);
-        try {
-            execute(outMess);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
+		outMess.setChatId(incomMessChatId);
+		outMess.setText(outMeString);
+		try {
+			execute(outMess);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public String getBotUsername() {
+	@Override
+	public String getBotUsername() {
 		return getenv.get("BOT_NAME");
-    }
+	}
 
-    @Override
-    public String getBotToken() {
-        return getenv.get("BOT_TOKEN");
-    }
+	@Override
+	public String getBotToken() {
+		return getenv.get("BOT_TOKEN");
+	}
 }
