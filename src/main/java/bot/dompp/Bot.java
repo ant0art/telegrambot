74b102b78c7702d19712c8ca1f8e91d2852f4788
Anton Filatov <antartbox@gmail.com>
2022-08-10 13:50:10 +0300

    Initial commit

--- src/main/java/bot/dompp/Bot.java
+++ src/main/java/bot/dompp/Bot.java
@@ -1,49 +1,77 @@
 package bot.dompp;
 
-// import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-// import org.telegram.telegrambots.meta.TelegramBotsApi;
-// import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-// import org.telegram.telegrambots.meta.api.objects.Message;
-// import org.telegram.telegrambots.meta.api.objects.Update;
-// import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-// import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class Bot {
+public class Bot extends TelegramLongPollingBot {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		// try {
-		// 	TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-		// 	telegramBotsApi.registerBot(new Bot());
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot(new Bot());
 
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+
+		// @Override
+		// public void onUpdateReceived(Update update) {
+		// Message incomMess = update.getMessage();
+		// String incomMessChatId = incomMess.getChatId().toString();
+		// String outMeString = incomMess.getText();
+		// SendMessage outMess = new SendMessage();
+
+		// outMess.setChatId(incomMessChatId);
+		// outMess.setText(outMeString);
+		// try {
+		// execute(outMess);
 		// } catch (TelegramApiException e) {
-		// 	e.printStackTrace();
+		// e.printStackTrace();
+		// }
+		// }
+
+		// @Override
+		// public String getBotUsername() {
+		// return "dom_pp_bot";
+		// }
+
+		// @Override
+		// public String getBotToken() {
+		// return "5595912289:AAEZ8vLLBtdQPX2phjsdEhiup_H5l6Ar-h8";
 		// }
 	}
 
-	// @Override
-	// public void onUpdateReceived(Update update) {
-	// 	Message incomMess = update.getMessage();
-	// 	String incomMessChatId = incomMess.getChatId().toString();
-	// 	String outMeString = incomMess.getText();
-	// 	SendMessage outMess = new SendMessage();
+	@Override
+	public void onUpdateReceived(Update update) {
+		Message incomMess = update.getMessage();
+		String incomMessChatId = incomMess.getChatId().toString();
+		String outMeString = incomMess.getText();
+		SendMessage outMess = new SendMessage();
 
-	// 	outMess.setChatId(incomMessChatId);
-	// 	outMess.setText(outMeString);
-	// 	try {
-	// 		execute(outMess);
-	// 	} catch (TelegramApiException e) {
-	// 		e.printStackTrace();
-	// 	}
-	// }
+		outMess.setChatId(incomMessChatId);
+		outMess.setText(outMeString);
+		try {
+		execute(outMess);
+		} catch (TelegramApiException e) {
+		e.printStackTrace();
+		}
 
-	// @Override
-	// public String getBotUsername() {
-	// 	return "dom_pp_bot";
-	// }
+	}
 
-	// @Override
-	// public String getBotToken() {
-	// 	return "5595912289:AAEZ8vLLBtdQPX2phjsdEhiup_H5l6Ar-h8";
-	// }
+	@Override
+	public String getBotUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBotToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
