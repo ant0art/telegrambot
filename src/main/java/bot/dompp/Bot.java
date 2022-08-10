e7c6d7c28867a6d6dab55a16a5c54afd233659e4
Anton Filatov <antartbox@gmail.com>
2022-08-10 15:18:04 +0300

    Initial commit

--- src/main/java/bot/dompp/Bot.java
+++ src/main/java/bot/dompp/Bot.java
@@ -1,53 +1,24 @@
 package bot.dompp;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Bot extends TelegramLongPollingBot {
 
-	public static void main(String[] args) {
-		try {
-			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-			telegramBotsApi.registerBot(new Bot());
+	String userName;
+	String token;
 
-		} catch (TelegramApiException e) {
-			e.printStackTrace();
-		}
-
-		// @Override
-		// public void onUpdateReceived(Update update) {
-		// Message incomMess = update.getMessage();
-		// String incomMessChatId = incomMess.getChatId().toString();
-		// String outMeString = incomMess.getText();
-		// SendMessage outMess = new SendMessage();
-
-		// outMess.setChatId(incomMessChatId);
-		// outMess.setText(outMeString);
-		// try {
-		// execute(outMess);
-		// } catch (TelegramApiException e) {
-		// e.printStackTrace();
-		// }
-		// }
-
-		// @Override
-		// public String getBotUsername() {
-		// return "dom_pp_bot";
-		// }
-
-		// @Override
-		// public String getBotToken() {
-		// return "5595912289:AAEZ8vLLBtdQPX2phjsdEhiup_H5l6Ar-h8";
-		// }
+	public Bot(String string, String string2) {
+		this.userName = string;
+		this.token = string2;
 	}
 
-	@Override
+    @Override
 	public void onUpdateReceived(Update update) {
+
 		Message incomMess = update.getMessage();
 		String incomMessChatId = incomMess.getChatId().toString();
 		String outMeString = incomMess.getText();
@@ -56,22 +27,19 @@ public class Bot extends TelegramLongPollingBot {
 		outMess.setChatId(incomMessChatId);
 		outMess.setText(outMeString);
 		try {
-		execute(outMess);
+			execute(outMess);
 		} catch (TelegramApiException e) {
-		e.printStackTrace();
+			e.printStackTrace();
 		}
-
 	}
 
 	@Override
 	public String getBotUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return userName;
 	}
 
 	@Override
 	public String getBotToken() {
-		// TODO Auto-generated method stub
-		return null;
+		return token;
 	}
 }
