c7f7a8b77bdd174be6d5f3d76efe369442f73c50
Anton Filatov <antartbox@gmail.com>
2022-08-18 02:16:34 +0300

    Add log config and props

--- src/main/java/bot/dompp/App.java
+++ src/main/java/bot/dompp/App.java
@@ -1,5 +1,6 @@
 package bot.dompp;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -9,13 +10,17 @@ import java.util.Map;
 public class App {
 	public static void main(String[] args) {
 
+
 		final Map<String, String> getenv = System.getenv();
+		PropertyConfigurator.configure("log4j.properties");
+
 
 		try {
 			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
 			botsApi.registerBot(new Bot(getenv.get("BOT_NAME"), getenv.get("BOT_TOKEN")));
 		} catch (TelegramApiException e) {
 			e.printStackTrace();
+
 		}
 	}
 }
