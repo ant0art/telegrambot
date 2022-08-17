dc35a81560d2c6394d016eb2e9bb85f733ae41e6
Anton Filatov <antartbox@gmail.com>
2022-08-18 02:14:22 +0300

    Change logic, clean up code, init new classes

--- src/main/java/bot/dompp/Bot.java
+++ src/main/java/bot/dompp/Bot.java
@@ -1,12 +1,21 @@
 package bot.dompp;
 
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage.SendMessageBuilder;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import bot.dompp.commands.BotCommandsConfig;
+import bot.dompp.commands.service.*;
 
-public final class Bot extends TelegramLongPollingBot {
+public final class Bot extends TelegramLongPollingCommandBot {
+	private Logger logger = LoggerFactory.getLogger(Bot.class);
 
 	private final String BOT_NAME;
 	private final String BOT_TOKEN;
@@ -15,63 +24,30 @@ public final class Bot extends TelegramLongPollingBot {
 		super();
 		this.BOT_NAME = botName;
 		this.BOT_TOKEN = botToken;
+		logger.debug("Name & Token are set");
 
-	}
+		/*
+		 * Список доступных команд. Не содержит scope
+		 */
+		register(new StartCommand("start", "Начнем с начала. Сервисная команда")); // service
+																					// command
+		register(new SearchCommand("search", "Список ключевых слов")); // service command
+		register(new HelpCommand(this)); // service command
 
-	@Override
-	public void onUpdateReceived(Update update) {
-
-		if (update.hasMessage() && update.getMessage().hasText()) {
-			// получаем сообщение полшьзователя
-			Message inputMessage = update.getMessage();
-
-			// формируем сообщение для ответа
-			SendMessage message = new SendMessage();
-			message.setChatId(inputMessage.getChatId());
-			message.setText(parseMessage(inputMessage));
-
-			try {
-				execute(message); // Call method to send the message
-			} catch (TelegramApiException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
 	// обработчик входящего сообщения
 	public String parseMessage(Message inputMessage) {
-		String userFirstName = inputMessage.getFrom().getFirstName();
-		String userLastName = inputMessage.getFrom().getLastName();
-		String userName = inputMessage.getFrom().getUserName();
-		String userAnswerName = "";
-
-		// обработчик обращения к пользователю по имени
-		if (userName == null) {
-			if (userLastName == null) {
-				userAnswerName = userFirstName;
-			} else
-				userAnswerName = userFirstName + " " + userLastName;
-		} else
-			userAnswerName = userName;
-
+		String userName = Utils.getUserName(inputMessage);
 		String response = String.format(
-				"%s, не спеши уходить! Я обязательно научусь выдавать тебе больше информации!",
-				userAnswerName);
-		// обработчик команд бота
+				"%s, не спеши уходить! Я обязательно научусь выдавать больше информации!",
+				userName);
 		if (inputMessage.isCommand()) {
-			switch (inputMessage.getText()) {
-				case "/start":
+			for (IBotCommand cmd : HelpCommand.getmCommandRegistry().getRegisteredCommands()) {
+				if (!inputMessage.getText().equals(cmd.getCommandIdentifier()))
 					response = String.format(
-							"Приветствую тебя, %s! Я - бот сообщества ЖК \"Полюстрово Парк\". %n Я нахожусь в процессе обучения",
-							userAnswerName);
-					break;
-				case "/help":
-					response = String.format("%s! Раздел помощи пока в разработке", userAnswerName);
-					break;
-				default:
-					response = String.format(
-							"%s! Эта команда мне пока неизвестна, выбери другую, пожалуйста",
-							userAnswerName);
+							"%s, Я подобной команды не знаю. Попробуй найти подходящую здесь /help",
+							userName);
 			}
 		}
 		return response;
@@ -85,5 +61,46 @@ public final class Bot extends TelegramLongPollingBot {
 	@Override
 	public String getBotToken() {
 		return BOT_TOKEN;
+	}
+
+	@Override
+	public void processNonCommandUpdate(Update update) {
+
+		// получаем сообщение полшьзователя
+		Message inMess = update.getMessage();
+		Long chatId = inMess.getChatId();
+		String response = parseMessage(inMess);
+
+		// проверяем входящие сущности
+		logger.warn("Here it is the log of all entities");
+		List<MessageEntity> entities = inMess.getEntities();
+		try {
+			entities.forEach(messageEntity -> logger.info(messageEntity.toString()));
+		} catch (NullPointerException e) {
+			logger.info(
+					String.format("This text is poor for any entity. Text: %s", inMess.getText()));
+		}
+		setAnswer(chatId, Utils.getUserName(inMess), response);
+	}
+
+	private void setAnswer(Long chatId, String userName, String text) {
+		// формируем сообщение для ответа
+		SendMessage message = new SendMessage();
+		SendMessageBuilder messageBuilder = SendMessage.builder();
+		SendMessage first =
+				messageBuilder.text("Вау! Что-то новенькое!").chatId(chatId.toString()).build();
+		message.enableMarkdown(true);
+		message.setChatId(chatId.toString());
+		message.setText(text);
+
+		try {
+			execute(new BotCommandsConfig().setBotCommands());
+			execute(message);
+			execute(first);
+		} catch (TelegramApiException e) {
+			logger.error(String.format("ERROR %s. Message doesn`t consist command. User: %s",
+					e.getMessage(), userName));
+			e.printStackTrace();
+		}
 	}
 }
